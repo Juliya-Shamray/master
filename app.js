c318e22fa0b02b420b833e6e7ae38a8ffb267527
Juliya Shamray <fizmatka908990@gmail.com>
2023-12-28 22:12:39 +0200

    settings

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
 
-const contactsRouter = require("./routes/api/contacts");
-const authRouter = require("./routes/api/users");
-
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
@@ -15,9 +12,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use(express.static("public"));
-
-app.use("/api/contacts", contactsRouter);
-app.use("/api/users", authRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
